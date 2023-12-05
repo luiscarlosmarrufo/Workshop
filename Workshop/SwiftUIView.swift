1011e5dba9cb0f3b20c957bca047657cf5766657
Luis Carlos <luiscarlosmarrufo@gmail.com>
2023-12-05 17:44:17 -0600

    First update

---
+++ Workshop/SwiftUIView.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftUIView.swift
+//  Workshop
+//
+//  Created by gdaalumno on 05/12/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        VStack {
+            Text("Luis Carlos")
+            Text("Me gusta Swift!")
+        }
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
