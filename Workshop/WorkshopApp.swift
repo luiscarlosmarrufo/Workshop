6a8005eb748cfdf279a1dedfa2379e5cd3f5d8c4
Luis Carlos <luiscarlosmarrufo@gmail.com>
2023-12-05 16:55:40 -0600

    Initial Commit

---
+++ Workshop/WorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkshopApp.swift
+//  Workshop
+//
+//  Created by gdaalumno on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
